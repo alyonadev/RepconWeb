750b04696c2c7998d9727e69c0845e58d533d0cc
alyonadev <kovalenkoalena789@gmail.com>
2020-06-02 18:46:03 +0300

    Smth

--- RepconWeb/Pages/Index.cshtml.cs
+++ RepconWeb/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,9 +10,15 @@ namespace RepconWeb.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        [BindProperty]
+        public Session SessionItem { get; set; }
+        public Classroom CrItem { get; set; }
+        public Process ProcItem { get; set; }
 
+        public IActionResult OnPost(string log, int crnum, int placenum, string proc, DateTime startT, DateTime endT)
+        {
+            return RedirectToPage("Report", new { log, crnum, placenum, proc, startT, endT });
         }
+
     }
 }
---
+++ RepconWeb/Pages/Report.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RepconWeb.Pages
+{
+    public class ReportModel : PageModel
+    {
+        private readonly RepconContext _context;
+        public List<Session> Sessions { get; set; }
+        public Session SessionItem { get; set; }
+        public async Task<IActionResult> OnGetAsync(string log, int crnum, int placenum, string proc, DateTime startT, DateTime endT)
+        {
+            //if (log == _context.Admn.Login)
+
+            
+
+            return Page();
+        }
+    }        
+}
